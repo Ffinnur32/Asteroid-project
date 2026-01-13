5dc8e1d50c4a376f9d9fd96e538646b4e40c54d4
Ffinnur32 <ffinnur32@gmail.com>
2026-01-13 10:53:45 +0000

    added while true statement for screen

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 import pygame
 from constants import SCREEN_WIDTH
@@ -11,6 +9,15 @@ def main():
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
