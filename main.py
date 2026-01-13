5d661919eed3cc694712b9bbc625e385db24fb3a
Ffinnur32 <ffinnur32@gmail.com>
2026-01-13 11:29:47 +0000

    added a fps clock

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -21,7 +25,9 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
-
+        ticks = clock.tick(60)
+        dt = ticks/1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
