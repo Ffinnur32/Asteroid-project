5fb12fedbb24f0ed87d0710abe467f7ef94ec85b
Ffinnur32 <ffinnur32@gmail.com>
2026-01-15 10:51:26 +0000

    player can now rotate left and right

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def main():
            
         screen.fill("black")
         player.draw(screen) 
+        player.update(dt)
         pygame.display.flip()
         ticks = clock.tick(60)
         dt = ticks/1000
