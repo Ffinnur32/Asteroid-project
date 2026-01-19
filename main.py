8289854248bd4476710b4bf628e9ea7cf807541f
Ffinnur32 <ffinnur32@gmail.com>
2026-01-19 14:25:55 +0000

    added the ability for player to shoot shots

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     VERSION = pygame.version.ver
@@ -21,14 +22,15 @@ def main():
     asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
 
     asteroid_field = AsteroidField()
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-
 
     while True:
         log_state()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from circleshape import CircleShape
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -43,4 +44,12 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
-    
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot_vector = pygame.Vector2(0,1)
+        rotated_shot_vector = shot_vector.rotate(self.rotation)
+        rotated_shot_vector_speed = rotated_shot_vector * PLAYER_SHOOT_SPEED
+        shot.velocity = rotated_shot_vector_speed
