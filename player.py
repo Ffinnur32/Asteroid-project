333d9abef6a98c23b7f9a9b04c7fbd7faffb4933
Ffinnur32 <ffinnur32@gmail.com>
2026-01-20 10:16:30 +0000

    added cooldown to shots to limit firerate

--- player.py
+++ player.py
@@ -1,4 +1,11 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import (
+    PLAYER_RADIUS, 
+    LINE_WIDTH, 
+    PLAYER_TURN_SPEED, 
+    PLAYER_SPEED, 
+    PLAYER_SHOOT_SPEED, 
+    PLAYER_SHOOT_COOLDOWN_SECONDS
+)
 from circleshape import CircleShape
 from shot import Shot
 import pygame
@@ -7,7 +14,7 @@ class Player(CircleShape):
     def __init__(self, x, y,):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
+        self.shot_cooldown = 0
     # in the Player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,6 +37,11 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def update(self, dt):
+        if self.shot_cooldown > 0:
+            self.shot_cooldown -= dt
+            if self.shot_cooldown < 0:
+                self.shot_cooldown = 0
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -48,8 +60,12 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.shot_cooldown > 0:
+                return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y)
         shot_vector = pygame.Vector2(0,1)
         rotated_shot_vector = shot_vector.rotate(self.rotation)
         rotated_shot_vector_speed = rotated_shot_vector * PLAYER_SHOOT_SPEED
         shot.velocity = rotated_shot_vector_speed
+
